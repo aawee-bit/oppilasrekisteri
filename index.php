3341f9ff72133bd1c3cab6ab762066938d5e9295
Khanistania <Hakanseppa@gmail.com>
2024-11-05 14:35:37 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oppilasrekisteri</title>
+</head>
+<body>
+    <nav class="header">
+
+    <a href="oppilasrekisteri.sql">Oppilasrekisteri</a>
+    <a href="db_connection.php">Lisää Oppilas</a>
+    <a href="handle_form.php">Hallinnoi tilejä</a>
+    <!-- Vaihtakaa "#" sitte oikeisii file nimiin -->
+    </nav>
+</body>
+</html>
