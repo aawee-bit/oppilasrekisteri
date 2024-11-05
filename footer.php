18fee86b70502dcd417cb3082f8c6609ac03af56
TyrForge <tyrforge.business@gmail.com>
2024-11-05 10:54:37 +0200

    Add files via upload
    Headerissä vaihtakaa "#" sitte oikeisii file nimiin että ne linkit toimii.

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer class="alaosa">
+
+    <p>&copy; 2024 Keski-Pohjanmaan ammattiopisto</p>
+    <p>Työ tehty 05/11/2024</p>
+
+</footer>
---
+++ header.php
@@ -0,0 +1,9 @@
+<nav class="header">
+
+    <a href="#">Oppilasrekisteri</a>
+    <a href="#">Lisää Oppilas</a>
+    <a href="#">Hallinnoi tilejä</a>
+
+    <!-- Vaihtakaa "#" sitte oikeisii file nimiin -->
+
+</nav>
