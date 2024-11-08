5f4799679ba7bd5b7699272908ee8515bd689a54
aawee-bit <83217925+aawee-bit@users.noreply.github.com>
2024-11-08 14:35:28 +0200

    Update index.php
    kirjoitusvirhe read_records linkissä

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
     <header>
     <nav class="header">
 
-    <a href="read_record.php">Oppilasrekisteri</a>
+    <a href="read_records.php">Oppilasrekisteri</a>
     <a href="create_form.php">Lisää Oppilas</a>
     <a href="update_record.php">Hallinnoi tilejä</a>
     <a href="delete_record.php">Poista tilejä</a>
