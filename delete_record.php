a14ae71aed70766034cb5529813f7d09be88dfc5
eliaispunk <eliateo.sandell@gmail.com>
2024-11-12 13:24:00 +0200

    Update delete_record.php

--- delete_record.php
+++ delete_record.php
@@ -19,6 +19,7 @@ $result = $conn->query($sql);
 <html lang="fi">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Oppilaslista</title>
     <link rel="stylesheet" href="style.css">
 </head>
